8b73ddcb2522afa892a6509e83c26216721c0ea0
Thao Nhu <thaonhu2710@yahoo.com>
2019-11-26 21:11:21 +0100

    1st commit

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+Recipe.create({ title: 'Bobun', cuisine:'vn'})
+.then(recipe => { console.log('The recipe is saved and its value is: ', recipe) })
+.catch(err => { console.log('An error happened:', err) });
+
+let promise1 = Recipe.insertMany(data);
+promise1.then(recipe => { console.log('The recipe is saved and its value is: ', recipe) })
+.catch(err => { console.log('An error happened:', err) });
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
+  .then(recipe => {console.log("successful update")})
+  .catch(err => { console.log('An error happened:', err) });
+
+Recipe.deleteOne({ title: "Carrot Cake"})
+  .then(recipe => {console.log("successful deletion")})
+  .catch(err => { console.log('An error happened:', err) });
+
+setTimeout(function (){mongoose.connection.close()}, 10000);
