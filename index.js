3aacb29445a1eb89c27ed95dd3c5720cf65e8df2
Sadigova Farida <55064917+ironhacker-sadigova@users.noreply.github.com>
2019-11-26 21:31:54 +0100

    nearly done

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
 const data = require('./data.js');  // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
 mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
   .then(() => {
     console.log('Connected to Mongo!');
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+/*Recipe.create({
+  title:'Master Cake',
+  level: 'Amateur Chef' ,
+  ingredients: ['chocolate', 'eggs', 'sugar', 'flour'],
+  cuisine: 'bakery',
+  dishType: 'Dessert',
+  image: 'https://images.media-allrecipes.com/images/75131.jpg',
+  duration: 60,
+  creator: 'Chef Pierre',
+  created: Date.now()
+})
+.then(recipe => {console.log(`The ${recipe.title} is added`)})
+.catch(err => {console.log(`An error happenned`,err)})*/
+
+
+Recipe.insertMany(data)
+.then(recipes=> {recipes.forEach(recipe => console.log(recipe.title))})
+.catch(err=> {console.log("Didn't work",err)})
+
+
+
+
+
+
+
 
