30b0817869bfc075ea44b18e2338b9256efa3b49
Viviajera <vivianajimenezh@gmail.com>
2019-11-26 21:36:19 +0100

    Commit until iteration 4 Viviana & Marina

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+//ITERATION 2
+/*Recipe.create ({title :'Ratatouille', cuisine: 'francaise'}, function(err, recipe){
+  if(err){
+    console.log ('An error had happened:', err); 
+  }else{
+    console.log ('The new recipe is: ', recipe);
+  }
+});*/
+
+// ITERATION 3
+/*Recipe.insertMany (data)
+.then((dbRes) => {
+  dbRes.forEach(recipe => console.log (recipe.title));
+  
+}).catch(err => {
+  console.error('Error connecting to mongo', err);
+});*/ 
+
+//ITERATION 4
+let successCallback = (recipe) => {
+  console.log('youpi')
+};
+let errorCallback = (err) => {};
+
+Recipe.updateOne ({title:'Rigatoni alla Genovese'}, {duration:100})
+.then(successCallback) 
+.catch(errorCallback); 
