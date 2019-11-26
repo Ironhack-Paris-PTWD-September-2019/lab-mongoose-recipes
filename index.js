a3484c443d64e72d758a091ec58aecbd9cec5b68
Sandrine8304 <sandrine.auberval@gmail.com>
2019-11-26 21:20:57 +0100

    iterations 1 à 4

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+// Recipe.create({title: 'Pizza', level: 'Easy Peasy', ingredients: ['pâte', 'sauce tomate', 'ananas', 'champignons', 'lardons', 'poivrons', 'oignons'], cuisine: 'Italienne', dishType: 'Dish', image: "/images/pizza-hawaienne.jpg", duration: 15, creator: 'Audrey & Sandrine', created: ''})
+//   .then(recipe => { console.log('A recipe has been created:', recipe.title)})
+//   .catch(err => { console.log('The recipe has not been created', err)});
+
+// Recipe.insertMany(data)
+// .then(recipe => { console.log(recipe.title)})
+// .catch(err => {console.log('The recipes have not been uploaded', err)});
+
+Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+.then(console.log('The Rigatoni alla Genovese duration has successfully been updated'))
+.catch(err => {console.log('The Rigatoni alla Genovese duration has not been updated')});
+
